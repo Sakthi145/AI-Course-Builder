732e9c62e02ede4827b93574e24b53f75b29b46a
demo <ushasakthi2003@gmail.com>
2026-05-18 13:45:20 +0530

    Add frontend URL to CORS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -132,8 +132,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOWED_ORIGINS = [
+    "https://ai-course-builder-dstsb5lu3-sakthi145s-projects.vercel.app",
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
